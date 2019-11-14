e9cf05083322a975d0d3cced6e16b306df6cf819
LeoKuhorev <kuhorev.leonid@gmail.com>
2019-11-13 16:20:45 -0800

    trail module added

--- modules/trail.js
+++ modules/trail.js
@@ -1,18 +1,30 @@
 'use strict';
 
-class Trail {
-  constructor(object) {
-    this.name = object.name;
-    this.location = object.location;
-    this.length = object.length;
-    this.stars = object.stars;
-    this.star_votes = object.starVotes;
-    this.summary = object.summary;
-    this.trail_url = object.url;
-    this.conditions = object.conditionStatus;
-    this.condition_date = object.conditionDate.slice(0, 10);
-    this.condition_time = object.conditionDate.slice(11);
-  }
+const path = require('path');
+const helperFunctions = require(path.join(__dirname, 'functions.js'));
+const fetchAPI = helperFunctions.fetchAPI;
+
+function Trail(object) {
+  this.name = object.name;
+  this.location = object.location;
+  this.length = object.length;
+  this.stars = object.stars;
+  this.star_votes = object.starVotes;
+  this.summary = object.summary;
+  this.trail_url = object.url;
+  this.conditions = object.conditionStatus;
+  this.condition_date = object.conditionDate.slice(0, 10);
+  this.condition_time = object.conditionDate.slice(11);
 }
 
-module.exports = Trail;
+exports.trailHandler = async function trailHandler(req, res) {
+  try {
+    const url = `https://www.hikingproject.com/data/get-trails?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${process.env.TRAIL_API_KEY}`;
+    const trailBody = await fetchAPI(url);
+    const trailData = trailBody.trails.map(element => new Trail(element));
+    res.status(200).send(trailData);
+  }
+  catch (error) {
+    console.log('Sorry, something went wrong', req, res);
+  }
+};
--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const path = require('path');
 
 // Bringing in modules
 const helperFunctions = require(path.join(__dirname, 'modules', 'functions.js'));
-
-const fetchAPI = helperFunctions.fetchAPI;
 const dbHandler = helperFunctions.dbHandler;
 
 const Location = require(path.join(__dirname, 'modules', 'location.js'));
@@ -24,6 +22,7 @@ const Event = require(path.join(__dirname, 'modules', 'events.js'));
 const eventsHandler = Event.eventsHandler;
 
 const Trail = require(path.join(__dirname, 'modules', 'trail.js'));
+const trailHandler = Trail.trailHandler;
 
 // Application setup
 const PORT = process.env.PORT || 3000;
@@ -43,24 +42,6 @@ app.get('/db', dbHandler);
 app.get('*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
-
-// Event Handlers
-
-async function trailHandler(req, res) {
-  try {
-    const url = `https://www.hikingproject.com/data/get-trails?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${process.env.TRAIL_API_KEY}`;
-    const trailBody = await fetchAPI(url);
-    const trailData = trailBody.trails.map(element => new Trail(element));
-    res.status(200).send(trailData);
-  }
-  catch (error) {
-    errorHandler('Sorry, something went wrong', req, res);
-  }
-}
-
-function errorHandler(error, req, res) {
-  res.status(500).send(error);
-}
 
 // Ensure that the server is listening for requests
 app.listen(PORT, () => console.log(`The server is up listening on ${PORT}`));
